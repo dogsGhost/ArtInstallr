ab0773a5d08a4d507114a938df287accaab86f10
David Wilhelm <davwilh@gmail.com>
2015-04-20 15:01:28 -0400

    canvas now updates with collection

--- src/js/components/ProjectCanvas.jsx
+++ src/js/components/ProjectCanvas.jsx
@@ -3,18 +3,19 @@ const React = require('react');
 module.exports = React.createClass({
   getInitialState() {
     return {
-      spacerWidth: this.setSpacerWidth(),
       scaleModifier: this.setScaleModifier()
     }
   },
 
+  // Initial rendering.
   componentDidMount() {
     this.renderPieces();
   },
 
-  componentWillUpdate() {
-    console.log('updating');
+  // Operate on DOM after changes.
+  componentDidUpdate() {
     var canvas = React.findDOMNode(this.refs.installation);
+    // Clear canvas and redraw pieces.
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     this.renderPieces();
   },
@@ -27,38 +28,33 @@ module.exports = React.createClass({
     return w > 1 ? parseInt(w, 10) : w;
   },
 
-  // getulates how much space should be between each piece.
-  setSpacerWidth() {
-    var _spacer = 0;
-
-    // Get combined width of all pieces.
-    this.props.collection.forEach((el) => _spacer += el.width);
-
-    // Subtract width of pieces from width of wall.
-    _spacer = this.props.settings.wallDims.width - _spacer;
-
-    // Divide by number of pieces plus one to account for space after last piece.
-    return parseInt(_spacer / (this.props.collection.length + 1), 10);
-  },
-
   // Draw pieces to canvas.
   renderPieces() {
     var canvas = React.findDOMNode(this.refs.installation);
     var _s = this.props.settings;
+    // Calculates how much space should be between each piece.
+    var spacer = 0;
+
+    // Get combined width of all pieces.
+    this.props.collection.forEach((el) => spacer += el.width);
+    // Subtract width of pieces from width of wall.
+    spacer = this.props.settings.wallDims.width - spacer;
+    // Divide by number of pieces plus one to account for space after last piece.
+    spacer = parseInt(spacer / (this.props.collection.length + 1), 10);
 
     // Confirm element is canvas element.
     if (canvas.getContext) {
       var ctx = canvas.getContext('2d');
       ctx.fillStyle = '#777';
-console.log('rendering', this.props.collection);
+
       // Loop through collection and draw piece to canvas.
       this.props.collection.forEach((piece, index) => {
         let h = piece.height * this.state.scaleModifier;
         let w = piece.width * this.state.scaleModifier;
         let y = (_s.wallDims.height - (_s.eyeLevel + (piece.height / 2))) * this.state.scaleModifier;
 
-        // Get left offset.      
-        let x = (index + 1) * this.state.spacerWidth;
+        // Get left offset.
+        let x = (index + 1) * spacer;
         // Add widths of all preceeding pieces.
         for (let i = 0; i < index; i++) {
           x += this.props.collection[i].width;
